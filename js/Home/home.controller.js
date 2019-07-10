742500cde7300041004d5e597782af97558bbbb3
aphilip97 <33058483+aphilip97@users.noreply.github.com>
2019-07-10 20:02:29 +0100

    Fixed temperatures by requesting for metric units
    - Latitude and longitude are now retrieved through the browser's
    Geolocation API.
    
    - This should let it work when deployed through GitHub Pages.

--- js/Home/home.controller.js
+++ js/Home/home.controller.js
@@ -11,40 +11,47 @@
     function control($http) {
 
         var vm = angular.extend(this, {
-            api_key: '3ae40bca12e2f190686e1d529ef34f00'
+            api_key: '3ae40bca12e2f190686e1d529ef34f00',
+            gotWeather: false,
+            units: "metric"
         });
 
         function getLocation() {
 
-            $http.get("http://ip-api.com/json").then(
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(
+                    position => {
+                        vm.lat = position.coords.latitude;
+                        vm.lon = position.coords.longitude;
+                    },
+                    err => {
+                        console.error(err);
+                    },
+                    {
+                        enableHighAccuracy: true
+                    }
+                );
+            } else {
+                console.error("Geolocation is not supported by this browser!");
+            }
 
-                function success(response) {
-                    vm.lat = response.data.lat;
-                    vm.lon = response.data.lon;
-                },
-
-                function failure(err) {
-                    console.log(err);
-                }
-
-            );
         }
 
         getLocation();
 
         vm.getWeather = function () {
 
-            var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + vm.lat + "&lon=" + vm.lon + "&appid=" + vm.api_key;
+            var url = `https://api.openweathermap.org/data/2.5/weather?lat=${vm.lat}&lon=${vm.lon}&units=${vm.units}&appid=${vm.api_key}`
 
             $http.get(url).then(
 
                 function success(response) {
-                    let data = response.data;
-                    vm.description = data.weather[0].description;
-                    vm.speed = (2.237 * data.wind.speed).toFixed(1) + " mph";
-                    vm.name = data.name;
-                    vm.temp = data.main.temp;
-                    vm.cTemp = (vm.temp - 32) * (5 / 9).toFixed(1) + " (ºC)";
+                    vm.description = response.data.weather[0].description.charAt(0).toUpperCase() + response.data.weather[0].description.slice(1);
+                    vm.windSpeed = (2.236936 * response.data.wind.speed).toFixed(1);
+                    vm.name = response.data.name;
+                    vm.tempCelsius = response.data.main.temp.toFixed(2);
+                    vm.tempFahrenheit = ((vm.tempCelsius * (9 / 5)) + 32).toFixed(2);
+                    vm.gotWeather = true;
                 },
 
                 function error(err) {
